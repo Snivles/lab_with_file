1618878502e93621671f5595cc26c65f7627f812
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-04-26 00:53:16 +0700

    реализация блочного кодирования и декодирования

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ unsigned char *ConvertToSmall(unsigned char*text)
 {
   if (text==NULL){return NULL;}
 
-  size_t dlina = 0;
+  int dlina = 0;
   while (text[dlina] != '\0')
         {dlina += 1;}
 
@@ -13,28 +13,33 @@ unsigned char *ConvertToSmall(unsigned char*text)
   unsigned char *result = (unsigned char*)malloc(dlina + 1); // выделяю память под результирующую строку
   int count = 0;
   size_t newc = 0;
-  int ostatok = dlina%8;
   int block = dlina / 8;
+  int blocks = 0;
   unsigned char simvol = 0;
-  while (count < block*8)
+  int maska=7 ;
+  while (blocks < block)
   {
-    if (count%8==0){
-          simvol = text[count];}
+    simvol = text[count];
 
-    else{
-      result[newc] = (text[count] | ( (simvol >> (7 - (count%8)))&1 )<<7);
+    int i = 0;
+    while (i < 7){
+      result[newc] = (text[count+i+1] | ( (simvol >> (7 - maska))&1 )<<7);
       newc++;
-
+      maska--;
+      i++;
       }
 
-    count++;
+    count += 8;
+    blocks += 1;
     }
-  if (ostatok!= 0){
-    for (int i = 0; i<ostatok;i++){
-        result[newc]= text[block * 8 + i];
-        newc++;}
 
-}
+
+  while (count < dlina){
+    result[newc] = text[count];
+    count++;
+    newc++;}
+
+
   result[newc] = '\0';
   return result;
 }
@@ -46,36 +51,45 @@ unsigned char *ConvertSMallto(unsigned char*text)
 {
   if (text==NULL){return NULL;}
 
-  size_t dlina = 0;
+  int dlina = 0;
   while (text[dlina] != '\0')
         {dlina += 1;}
   int block = dlina / 7;
-//8 8 8 4 = 28
-//7 7 7 4 = 25
+
+
+
   unsigned char *result = (unsigned char*)malloc((dlina + block) + 1); // выделяю память под результирующую строку
   int count = 0;
   size_t newc = 0;
-  unsigned char simvol = 0;
-  while (count < block*8)
+  int blocks = 0;
+  unsigned char maska = 0;
+  unsigned char mask = 0;
+  mask = (mask << 7);
+  while (blocks < block)
   {
-    if (count%8==0){
-          simvol = text[count];}
+    maska = 0;
+    int i = 0;
+    while (i < 7){
+          maska = maska | ((text[count+i]>>7)&1) << i;
+          i++;}
+    result[newc] = maska;
+    newc++;
 
-    else{
-      result[newc] = (text[count] | ( (simvol >> (7 - (count%8)))&1 )<<7);
+    i = 0;
+    while(i < 7){
+      result[newc] = (text[count]& ~mask);
       newc++;
-
-      }
-
-    count++;
-    }
-  int ostatok = (dlina+block)-(block*7);
-  if (ostatok!= 0){
-    for (int i = 0; i<ostatok;i++){
-        result[newc]= text[block * 8 + i];
-        newc++;}
-
+      i++;}
+    count += 7;
+    blocks += 1;
 }
+
+
+  while (count < dlina){
+    result[newc] = text[count];
+    count++;
+    newc++;}
+
   result[newc] = '\0';
   return result;
 }
@@ -87,14 +101,21 @@ unsigned char *ConvertSMallto(unsigned char*text)
 
 int main()
 {
-  unsigned char text[1000] = "zaaaaaaaffffffff";
-  unsigned char *ptr = text;
-  ptr = ConvertToSmall(ptr);
+  unsigned char text[1000] = "zaaaaaaaffff";
+  unsigned char *ptr = ConvertToSmall(text);
   int i =0;
   while (ptr[i] != '\0'){
     printf("%c",ptr[i]);
-    i++;
-}
-
+    printf("\n");
+    i++;}
+  i =0;
+  printf("\n");
+  unsigned char *ptr2 = ConvertSMallto(ptr);
+  while (ptr2[i] != '\0'){
+    printf("%c",ptr2[i]);
+    printf("\n");
+    i++;}
+  free(ptr);
+  free(ptr2);
   return 0;
 }
